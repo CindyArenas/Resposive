c2c0414cbcfbac7154ce281feb49ccc40fbd33c4
CindyArenas <cindyarenas9@gmail.com>
2022-10-25 18:40:57 -0500

    25-10/22

---
+++ clase_1/script.js
@@ -0,0 +1,37 @@
+/*let h = "Pato";
+let p = "Ganzo";
+let j = "Javascript is amazin"
+
+console.log (h + " is " + p  )*/
+
+/*function tecla (pulsame){
+
+if (tecla === 'number' ){
+    console.log (pulsame)
+}
+else {
+    console.log ("No es un numero")
+}
+}
+
+
+
+/*function tecla  (pulsame) { 
+if ( pulsame === undefined ) {
+    console.log ("si")
+
+}
+else {
+    console.log(pulsame)
+}
+
+
+}*/
+function tecla (pulsame){
+
+while ( pulsame<=3){
+    console.log ('abc')
+}
+ do {
+    console.log ('no')
+ }
